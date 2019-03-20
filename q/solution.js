c8ca80afc8ed52c04654bbeca5228b00cd71457a
Steven Inouye <steveninouye@msn.com>
2019-03-19 17:20:39 -0700

    Create initial queues for message delivery service

--- q/solution.js
+++ q/solution.js
@@ -1,29 +1,37 @@
-
+const Queue = require('queue-fifo');
 
 // This is an example of a terrible "solution" to the Q problem.
 
 // You may delete all of the contents of this file to start fresh.
 // Please do be sure, however, that your solution provides at least these
 // functions so that we can test it.
-class Terrible {
+class MessageDeliveryService {
   constructor() {
-    this.msgQueue = [];
+    this.queues = [];
+    this.createQueues();
   }
+
+  createQueues() {
+    for (let i = 0; i < 5; i++) {
+      this.queues.push(new Queue());
+    }
+  }
+
   enqueue(msg) {
     this.msgQueue.push(msg);
   }
   next(queueNumber) {
-    return this.msgQueue.shift()
+    return this.msgQueue.shift();
   }
 }
-    
+
 // Regardless of how you choose to implement your solution, please do something
 // like this so that we can get a clean instance of your solution by calling
 // solution.getMessageService().
 const getMessageService = () => {
-  return new Terrible()
-}
-    
+  return new MessageDeliveryService();
+};
+
 module.exports = {
   getMessageService
-}
+};
